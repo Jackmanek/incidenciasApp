5c842d13bddde82d354d50d94f03f0d3d6f82ee5
Jasio <age002@gmail.com>
2024-10-30 10:31:27 +0100

    aniadido columna roles y modificado gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +22,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('gestor-incidencias', function($user){
+            return $user->role === 'administrador';
+        });
+
+        Gate::define('incidencia-asignada', function($user){
+            return $user->role === 'soporte';
+        });
     }
 }
